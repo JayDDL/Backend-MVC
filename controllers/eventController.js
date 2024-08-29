bd188bb3df22ffdd26abcec571a69da349cc0e45
JayDDL <dlittle.cdiem@gmail.com>
2024-08-29 15:16:26 +0200

    [Feat] Implemented eventController.js

---
+++ controllers/eventController.js
@@ -0,0 +1,33 @@
+const eventModel = require('../models/eventModel');
+
+// Add an event
+function addEvent(req, res) {
+  const { name, location, date } = req.body;
+  if (!name || !location || !date) {
+    return res.status(400).send('All fields are required');
+  }
+
+  const event = { name, location, date };
+  eventModel.addEvent(event);
+  res.status(201).send('Event added successfully');
+}
+
+// Retrieve events by date
+function getEventsByDate(req, res) {
+  const { date } = req.params;
+  const events = eventModel.getEventsByDate(date);
+  res.json(events);
+}
+
+// Retrieve events by location
+function getEventsByLocation(req, res) {
+  const { location } = req.params;
+  const events = eventModel.getEventsByLocation(location);
+  res.json(events);
+}
+
+module.exports = {
+  addEvent,
+  getEventsByDate,
+  getEventsByLocation
+};
